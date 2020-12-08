af0046161aaeeb3b014898b11f01049bbe01476d
coderrick <derrick.duncan@temple.edu>
2020-12-07 19:21:13 -0500

    BirthDateHelper returns time difference

--- api_server.go
+++ api_server.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"text/template"
+	"time"
 )
 
 //HTML login form and form data
@@ -18,25 +20,45 @@ func RenderForm(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//Obfuscate patient info in form data
 func DeIndentify(w http.ResponseWriter, r *http.Request) {
 	//Get data from html login form
 	r.ParseForm()
-	// a := Helper(r.Form["birthdate"])
-	// fmt.Println(a)
-	fmt.Println("BirthDate:", Helper(r.Form["birthdate"]))
-	fmt.Println("ZipCode:", Helper(r.Form["zipcode"]))
-	fmt.Println("Admission Date:", Helper(r.Form["admissiondate"]))
-	fmt.Println("Discharge Date:", Helper(r.Form["dischargedate"]))
-	fmt.Println("Notes:", Helper(r.Form["notes"]))
+	fmt.Println("BirthDate:", BirthDateHelper(r.Form["birthdate"]))
+	fmt.Println("ZipCode:", ZipCodeHelper(r.Form["zipcode"]))
+	fmt.Println("Admission Date:", DateHelper(r.Form["admissiondate"]))
+	fmt.Println("Discharge Date:", DateHelper(r.Form["dischargedate"]))
+	fmt.Println("Notes:", NotesHelper(r.Form["notes"]))
 }
 
-func Helper(data []string) string {
+//Parse birthdate form data
+func BirthDateHelper(data []string) string {
 	//convert from []string to string
-	res := strings.Join(data, " ")
-	return res
+	bd := strings.Join(data, " ")
+	then, err := time.Parse("2006-01-02", bd)
+	if err != nil {
+		fmt.Println(err)
+	}
+	dur := time.Since(then)
+	n := uint64(dur.Hours() / 8760)
+	age := strconv.FormatUint(uint64(n), 10)
+	return age
+}
+
+func ZipCodeHelper(data []string) string {
+	//TODO
+}
+
+func DateHelper(data []string) string {
+	//TODO
+}
+
+func NotesHelper(data []string) string {
+	//TODO
 }
 
 func main() {
+	fmt.Println(time.Now().Format("2006-01-02"))
 	log.Print("Listening on Port 8080; http://localhost:8080")
 	http.HandleFunc("/", RenderForm) // setting router rule
 	http.HandleFunc("/deidentify", DeIndentify)

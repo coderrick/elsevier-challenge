c9fcad0eb326b7928dee6d61980761559d3d410c
coderrick <derrick.duncan@temple.edu>
2020-12-08 23:02:29 -0500

    Added test for ZipCodeHelper

--- api_server_test.go
+++ api_server_test.go
@@ -4,8 +4,16 @@ import "testing"
 
 func TestBirthDateHelper(t *testing.T) {
 	a := []string{"2000-01-01"}
-	total := BirthDateHelper(a)
-	if total != "20" {
-		t.Errorf("BirthDate was incorrect, got: %s, want: %s.", total, "10")
+	result := BirthDateHelper(a)
+	if result != "20" {
+		t.Errorf("BirthDate was incorrect, got: %s, want: %s.", result, "20")
+	}
+}
+
+func TestZipCodeHelper(t *testing.T) {
+	a := []string{"10013"}
+	result := ZipCodeHelper(a)
+	if result != "10000" {
+		t.Errorf("ZipCode was incorrect, got: %s, want: %s.", result, "10000")
 	}
 }

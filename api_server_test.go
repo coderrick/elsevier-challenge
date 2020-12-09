6e1e80013dc87cd584f1a1d04ea8af60f9594747
coderrick <derrick.duncan@temple.edu>
2020-12-08 23:10:24 -0500

    Added test for NotesHelper

--- api_server_test.go
+++ api_server_test.go
@@ -22,6 +22,14 @@ func TestDateHelper(t *testing.T) {
 	a := []string{"2019-03-12"}
 	result := DateHelper(a)
 	if result != "2019" {
-		t.Errorf("ZipCode was incorrect, got: %s, want: %s.", result, "2019")
+		t.Errorf("Admission or Discharge Date was incorrect, got: %s, want: %s.", result, "2019")
+	}
+}
+
+func TestNotesHelper(t *testing.T) {
+	a := []string{"Patient with ssn 123-45-6789 previously presented under different ssn"}
+	result := NotesHelper(a)
+	if result != "Patient with ssn XXX-XX-XXXX previously presented under different ssn" {
+		t.Errorf("ZipCode was incorrect, got: %s, want: %s.", result, "Patient with ssn XXX-XX-XXXX previously presented under different ssn")
 	}
 }
